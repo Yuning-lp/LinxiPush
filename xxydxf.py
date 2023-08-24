fd60f79990774c5f5c75839899ad95c390e3cd1b
林夕 <75199709+linxi-520@users.noreply.github.com>
2023-08-24 13:35:25 +0800

    Update xxydxf.py

--- xxydxf.py
+++ xxydxf.py
@@ -1,15 +1,28 @@
 # Author: lindaye
-# update: 2023-08-21 14:00
+# update: 2023-08-24 13:32
 # 1.修复提现
 # 2.新增多账户
+# 3.新增100篇推送信息
+# 4.优化代码
 # 小小阅读
+# 微信测试号: https://s1.ax1x.com/2023/08/23/pPJ5bnA.png
 # 入口: https://fia.douyifang.top:10263/yunonline/v1/auth/1c3da9bd1689d78a51463138d634512f?codeurl=fia.douyifang.top:10263&codeuserid=2&time=1692525245
+# 使用教程: 1.填入uid_list值(仅需ysm_uid=后的内容) 2.扫码关注微信测试号 3.填写微信昵称
+# V1.4(测试版)
 
 import requests
 import re
 import time
 import random
 
+# 保持连接,重复利用
+ss = requests.session()
+# 推送域名
+tsurl = 'https://linxi-send.run.goorm.app'
+# 临时用户名
+temp_user = ""
+# 微信昵称
+wxname = 'XX'
 # 仅填写uid_list内容即可(抓包获取Cookie中的ysm_uid的值填入##)
 # 单账号 uid_list = ['##']
 # 多账号 uid_list = ['##','##']
@@ -28,8 +41,7 @@ def ts ():
 
 
 def signin():
-    url = 'http://1692416143.3z2rpa.top/'
-    result = requests.get(url,headers=headers).text
+    result = ss.get('http://1692416143.3z2rpa.top/',headers=headers).text
     signid = re.findall(r'id\'\) \|\| "(.*?)";',result)
     if signid == []:
         print ('初始化失败,账号异常')
@@ -39,8 +51,7 @@ def signin():
 
 
 def get_money(signid):
-    url = f'http://1692429080.3z2rpa.top/yunonline/v1/exchange?unionid={ysm_uid}&request_id={signid}&qrcode_number=&addtime='
-    result = requests.get(url,headers=headers).text
+    result = ss.get(f'http://1692429080.3z2rpa.top/yunonline/v1/exchange?unionid={ysm_uid}&request_id={signid}&qrcode_number=&addtime=').text
     money = re.findall(r'id="exchange_gold">(.*?)</p>',result)
     if money == []:
         print ('金币获取失败,账号异常')
@@ -48,41 +59,36 @@ def get_money(signid):
         if int(money[0]) >= 3000:
             money = (int(money[0]) // 3000) * 3000
             print(f"提交体现金币: {money}")
-            t_url = 'http://1692429080.3z2rpa.top/yunonline/v1/user_gold'
             t_data = {'unionid':ysm_uid,'request_id':signid,'gold':money}
-            t_result = requests.post(t_url,headers=headers,json=t_data).json()
+            t_result = ss.post('http://1692429080.3z2rpa.top/yunonline/v1/user_gold',json=t_data).json()
             if t_result['errcode'] == 0:
                 print(f"金币转金额成功: {t_result['data']['money']}")
             else:
                 print(f"金币转金额失败: {t_result['msg']}")
-            j_url = 'http://1692422733.3z2rpa.top/yunonline/v1/withdraw'
             j_data = {'unionid':ysm_uid,'signid':signid,'ua':0,'ptype':0,'paccount':'','pname':''}
-            j_result = requests.post(j_url,headers=headers,data=j_data).json()
-            print(f"体现结果: {j_result}")
+            j_result = ss.post('http://1692422733.3z2rpa.top/yunonline/v1/withdraw',data=j_data).json()
+            print(f"体现结果: {j_result['msg']}")
         else:
             print(f'还未到达提现最低金币 当前金币: {money[0]}')
 
 
 
 def user_info():
-    url = f'http://1692416143.3z2rpa.top/yunonline/v1/sign_info?time={ts()}000&unionid={ysm_uid}'
-    result = requests.get(url,headers=headers).json()
+    result = ss.get(f'http://1692416143.3z2rpa.top/yunonline/v1/sign_info?time={ts()}000&unionid={ysm_uid}').json()
     if result['errcode'] == 0:
         pass
     else:
         print ('获取用户信息失败，账号异常')
 
 def hasWechat():
-    url = f'http://1692416143.3z2rpa.top/yunonline/v1/hasWechat?unionid={ysm_uid}'
-    result = requests.get(url,headers=headers).json()
+    result = ss.get(f'http://1692416143.3z2rpa.top/yunonline/v1/hasWechat?unionid={ysm_uid}').json()
     if result['errcode'] == 0:
         pass
     else:
         print ('获取用户信息失败，账号异常')
 
 def gold():
-    url = f'http://1692416143.3z2rpa.top/yunonline/v1/gold?unionid={ysm_uid}&time={ts()}000'
-    result = requests.get(url,headers=headers).json()
+    result = ss.get(f'http://1692416143.3z2rpa.top/yunonline/v1/gold?unionid={ysm_uid}&time={ts()}000').json()
     if result['errcode'] == 0:
         print(f"今日积分: {result['data']['day_gold']} 已阅读: {result['data']['day_read']}篇 剩余: {result['data']['remain_read']}篇")
     else:
@@ -90,41 +96,91 @@ def gold():
 
 
 def get_Key():
-    url = 'http://1692416143.3z2rpa.top/yunonline/v1/wtmpdomain'
     data = {'unionid':ysm_uid}
-    result = requests.post(url,headers=headers,json=data).json()
+    result = ss.post('http://1692416143.3z2rpa.top/yunonline/v1/wtmpdomain',json=data).json()
     uk = re.findall(r'uk=([^&]+)',result['data']['domain'])
     print(f"获取到KEY: {uk[0]}")
     do_read(uk[0])
 
 def do_read(uk):
+    check_read = 0
     while True:
-        url = f'https://nsr.zsf2023e458.cloud/yunonline/v1/do_read?uk={uk}'
-        result = requests.get(url,headers=headers).json()
+        result = ss.get(f'https://nsr.zsf2023e458.cloud/yunonline/v1/do_read?uk={uk}').json()
         if result['errcode'] == 0:
             link = result['data']['link']
-            l_result = requests.get(link,headers=headers,allow_redirects=False).status_code
-            s = random.randint(7,15)
-            print (f'本次模拟读{s}秒')#line:141
-            time.sleep(s)
-            rurl = f'https://nsr.zsf2023e458.cloud/yunonline/v1/get_read_gold?uk={uk}&time={s}&timestamp={ts()}'
-            r_result = requests.get(rurl,headers=headers).json()
-            if r_result['errcode'] == 0:
-                print(f"阅读已完成: 获得{r_result['data']['gold']}积分")
+            l_result = ss.get(link,headers=headers).text
+            s = random.randint(6,8)
+            print (f'获取{check_read}文章成功,本次模拟读{s}秒')
+            if check_read == 99:
+                print("阅读文章检测--100篇检测---已推送至微信")
+                link = re.findall('msg_link = "(.*?)";',l_result)[0]
+                # 过检测
+                check = test("100",link)
+                if check == True:
+                    print("检测文章-过检测成功啦!")
+                    r_result = ss.get(f'https://nsr.zsf2023e458.cloud/yunonline/v1/get_read_gold?uk={uk}&time={s}&timestamp={ts()}').json()
+                    if r_result['errcode'] == 0:
+                        print(f"阅读已完成: 获得{r_result['data']['gold']}积分")
+                        check_read = r_result['data']['day_read']
+                    else:
+                        print(r_result)
+                        break
+                else:
+                    print("检测文章-过检测失败啦!")
+                    break
             else:
-                print(f"阅读失败: {r_result},重新获取阅读")
+                time.sleep(s)
+                r_result = ss.get(f'https://nsr.zsf2023e458.cloud/yunonline/v1/get_read_gold?uk={uk}&time={s}&timestamp={ts()}').json()
+                if r_result['errcode'] == 0:
+                    print(f"阅读已完成: 获得{r_result['data']['gold']}积分")
+                    check_read = r_result['data']['day_read']
+                else:
+                    print(r_result)
+                    break
         else:
             if result['msg'] == "任务重复":
                 print("阅读失败: 阅读重复重新获取文章")
             else:
                 print (f"阅读提醒: {result['msg']}")
                 break
-   
+
+
+def test(biz,link):
+    result = ss.post(tsurl+"/task",json={"biz":temp_user+biz,"url":link}).json()
+    WxSend("微信阅读-小阅阅读", "检测文章", "请在30秒内完成当前文章",tsurl+"/read/"+temp_user+biz)
+    check = ''
+    for i in range(30):
+        result = ss.get(tsurl+"/back/"+temp_user+biz).json()
+        if result['status'] == True:
+            check = True 
+            break
+        else:
+            print("等待检测中...", end="\r", flush=True)
+        time.sleep(1)
+    if result['status'] == False:
+        print("手动检测超时,验证失败!")
+        check = False 
+    return check
+
+
+# 微信推送
+def WxSend(project, status, content,turl):
+    data = {
+        "name": wxname, # 微信昵称
+        "project": project,
+        "status": status,
+        "content": content,
+        "url":turl
+    }
+    result = ss.post(tsurl, json=data).json()
+    print(f"微信消息推送: {result['msg']}")
+
 
 print(f"=================获取到{len(uid_list)}个账号==================")
 for id in range(len(uid_list)):
     print(f"当前为第{id+1}个账号")
     ysm_uid = uid_list[id]
+    temp_user = ysm_uid
     headers['Cookie'] = f'ysm_uid={ysm_uid}'
     signid = signin()
     user_info()
